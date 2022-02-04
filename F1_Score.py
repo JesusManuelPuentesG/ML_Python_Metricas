351f02434ea9ec804465958f877af2d9b87c6795
JesusManuelPuentesG <99046961+JesusManuelPuentesG@users.noreply.github.com>
2022-02-04 22:03:37 +0100

    Update F1_Score.py

--- F1_Score.py
+++ F1_Score.py
@@ -1,7 +1,12 @@
 …
+
 # Librería necesaria para utilizar la métrica F1-Score
 from sklearn.metrics import f1_score
+
 …
+
 f1 = f1_score(y_test, yhat_classes)
+
 print('F1 score - 2 tp / (2 tp + fp + fn): \t%f' % f1)
+
 …
