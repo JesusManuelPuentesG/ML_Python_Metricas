e576d964c29c951ba87e1b23616a2991fa6002d8
JesusManuelPuentesG <99046961+JesusManuelPuentesG@users.noreply.github.com>
2022-02-04 22:03:13 +0100

    Update Exactitud_Precision.py

--- Exactitud_Precision.py
+++ Exactitud_Precision.py
@@ -1,7 +1,12 @@
 …
+
 # Librería necesaria para utilizar la métrica Precision
 from sklearn.metrics import precision_score
+
 …
+
 precision = precision_score(y_test, yhat_classes)
+
 print('Precision - tp / (tp + fp): \t\t%f' % precision)
+
 …
