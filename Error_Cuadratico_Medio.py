1ed322ec9ed2b0c0831433e527cf62d1d55ff0c7
JesusManuelPuentesG <99046961+JesusManuelPuentesG@users.noreply.github.com>
2022-02-04 22:02:37 +0100

    Update Error_Cuadratico_Medio.py

--- Error_Cuadratico_Medio.py
+++ Error_Cuadratico_Medio.py
@@ -1,7 +1,12 @@
 …
+
 # Librería necesaria para utilizar la métrica Error Cuadrático Medio
 from sklearn.metrics import mean_squared_error
+
 …
+
 mse = mean_squared_error(testy[:,0], ypred[:,0])
+
 print('Error Cuadrático Medio: \t%f' % mse)
+
 …
