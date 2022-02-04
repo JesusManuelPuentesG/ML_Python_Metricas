4c965619a2c4a2c4fb0f2e9082831bae43835c89
JesusManuelPuentesG <99046961+JesusManuelPuentesG@users.noreply.github.com>
2022-02-04 22:02:02 +0100

    Update Error_Absoluto_Medio.py

--- Error_Absoluto_Medio.py
+++ Error_Absoluto_Medio.py
@@ -1,7 +1,12 @@
 …
+
 # Librería necesaria para utilizar la métrica Error Absoluto Medio
 from sklearn.metrics import mean_absolute_error
+
 …
+
 mae = mean_absolute_error (testy[:,0], ypred[:,0])
+
 print('Error Cuadrático Medio: \t%f' % mae)
+
 …
